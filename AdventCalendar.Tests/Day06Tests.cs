63deaf8fe708e8331baed2c1f7bc5ab4ecaf4888
Qwx <qwxmail@gmail.com>
2019-01-04 00:16:05 +0200

    Day06Puzzle02:Getting ready to generate fields

---
+++ AdventCalendar.Tests/Day06Tests.cs
@@ -0,0 +1,99 @@
+﻿using AdventCalendar.Day06;
+using AutoFixture.Xunit2;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AdventCalendar.Tests
+{
+    public class Day06Tests
+    {
+        [Theory(Skip = "Later"), AutoData]
+        public void Day06Puzzle01_LargestNonInfiniteArea(Mock<IInputReader> inputReaderMock)
+        {
+            inputReaderMock.Setup(x => x.ReadLines()).Returns(new List<string>
+            {
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9",
+            });
+
+            var puzzle = new Day06Puzzle01(inputReaderMock.Object);
+
+            var result = puzzle.Solve();
+
+            Assert.Equal("17", result);
+        }
+
+        [Fact]
+        public void BoardGetsMaxValues()
+        {
+            var sources = new List<string>
+            {
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9",
+            }.Select(Source.Parse);
+            var boardFactory = new BoardFactory(sources);
+
+            (var maxX, var maxY) = boardFactory.GetMaxXY();
+
+            Assert.Equal(8, maxX);
+            Assert.Equal(9, maxY);
+        }
+
+        [Theory]
+        [InlineData("1, 1")]
+        [InlineData("1, 6")]
+        [InlineData("8, 3")]
+        [InlineData("3, 4")]
+        [InlineData("5, 5")]
+        [InlineData("8, 9")]
+        public void FieldGenerator_SetsCoordinateUnderTheSource(string source)
+        {
+            var sources = new List<string>
+            {
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9",
+            }.Select(Source.Parse);
+            var generator = new FieldGenerator(sources);
+
+            var fields = generator.CreateFields();
+
+            var expectedSource = Source.Parse(source);
+            Assert.Equal(expectedSource, fields[expectedSource.X][expectedSource.Y].Source);
+            Assert.Equal(0, fields[expectedSource.X][expectedSource.Y].Distance);
+        }
+
+        [Fact(Skip = "later")]
+        public void FieldGenerator_SetsCoordinateClosestToOneSource()
+        {
+            var sources = new List<string>
+            {
+                "1, 1",
+                "1, 6",
+                "8, 3",
+                "3, 4",
+                "5, 5",
+                "8, 9",
+            }.Select(Source.Parse);
+            var generator = new FieldGenerator(sources);
+
+            var fields = generator.CreateFields();
+
+            Assert.Equal(Source.Parse("1, 1"), fields[0][0].Source);
+            Assert.Equal(1, fields[0][0].Distance);
+        }
+    }
+}
---
+++ AdventCalendar/Day06/Day06Puzzle01.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventCalendar.Day06
+{
+    internal class Day06Puzzle01 : Puzzle
+    {
+        public Day06Puzzle01(IInputReader inputReader) : base(inputReader)
+        {
+        }
+
+        public override string Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class BoardFactory
+    {
+        private readonly IEnumerable<Source> _sources;
+
+        public BoardFactory(IEnumerable<Source> sources)
+        {
+            _sources = sources;
+        }
+
+        public (int, int) GetMaxXY()
+        {
+            return (_sources.Max(p => p.X), _sources.Max(p => p.Y));
+        }
+    }
+
+    internal class Source
+    {
+        public Source(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public static Source Parse(string line)
+        {
+            var elements = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var x = elements[0];
+            var y = elements[1];
+
+            return new Source(int.Parse(x), int.Parse(y));
+        }
+
+        protected bool Equals(Source other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (X * 397) ^ Y;
+            }
+        }
+    }
+
+    internal class Vector
+    {
+        public Vector(Source source, int distance)
+        {
+            Source = source;
+            Distance = distance;
+        }
+
+        public Source Source { get; }
+        public int Distance { get; }
+    }
+
+    internal class FieldGenerator
+    {
+        private readonly IEnumerable<Source> _sources;
+
+        public FieldGenerator(IEnumerable<Source> sources)
+        {
+            _sources = sources;
+        }
+
+        public Vector[][] CreateFields()
+        {
+            (var maxX, var maxY) = new BoardFactory(_sources).GetMaxXY();
+            var board = CreateBoard(maxX, maxY);
+
+            foreach (var source in _sources)
+            {
+                board[source.X][source.Y] = new Vector(source, 0);
+            }
+
+            return board;
+        }
+
+        private static Vector[][] CreateBoard(int maxX, int maxY)
+        {
+            var fields = new Vector[maxX + 1][];
+            for (var x = 0; x <= maxX; x++)
+            {
+                fields[x] = new Vector[maxY + 1];
+            }
+
+            return fields;
+        }
+    }
+}
